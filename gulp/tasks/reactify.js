8f5a35f28c57dac3cb02a42641441124b8050ff4
jkrayer <jameskrayer@yahoo.com>
2015-10-01 22:30:46 -0400

    Add babel to build

--- gulp/tasks/reactify.js
+++ gulp/tasks/reactify.js
@@ -1,15 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
+var gulpBabel = require('gulp-babel');
 var gulpReact = require('gulp-react');
 var jshint = require('gulp-jshint');
 
 gulp.task('reactify', function() {
   gulp.src('./src/js/**/*')
     .pipe(gulpReact())
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'))
+    .pipe(gulpBabel())
+//    .pipe(jshint())
+//    .pipe(jshint.reporter('default'))
+//    .pipe(jshint.reporter('fail'))
     .pipe(gulp.dest('./dist/js'));
 });
 
