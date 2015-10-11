54024157ea19bb973f059b8c598bb915746167dc
jkrayer <jameskrayer@yahoo.com>
2015-10-11 11:39:20 -0400

    Add SASS/CSS to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
 var webpack = require('webpack');
 
-module.exports = {
-  entry: {
-    app: ['./src/js/myapp.jsx']
+module.exports = [
+  {
+    entry: {
+      app: ['./src/js/myapp.jsx']
+    },
+    output: {
+      path: './dist/js',
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    }
   },
-  output: {
-    path: './dist/js',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
+  {
+    entry: {
+      app: ['./src/css/style.scss']
+    },
+    output: {
+      path: './dist/css',
+      filename: 'style.css'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.scss$/,
+          loaders: ['style', 'css', 'sass']
+        }
+      ]
+    }
   }
-};
+];
