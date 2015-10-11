b8880086c6a401eeebd17af7158eab78c6b905b6
jkrayer <jameskrayer@yahoo.com>
2015-10-11 17:40:47 -0400

    Add ref to style for webpack

--- src/js/myapp.jsx
+++ src/js/myapp.jsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import Data from '../../../data/monsters';
 
+import "../css/style";
+
 ReactDOM.render(<App allMonsters={Data} />, document.getElementById('mountpoint'));
