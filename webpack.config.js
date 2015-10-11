0b070287999468981517bb63b70f9c3b925ca428
jkrayer <jameskrayer@yahoo.com>
2015-10-11 17:41:49 -0400

    Refactor to preferred output

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,33 @@
-module.exports = [
-  {
-    entry: {
-      app: ['./src/js/myapp.jsx']
-    },
-    output: {
-      path: './dist/js',
-      filename: 'bundle.js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          loader: 'babel'
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    }
-  },
-  {
-    entry: {
-      app: ['./src/css/style.scss']
-    },
-    output: {
-      path: './dist/css',
-      filename: 'style.css'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.scss$/,
-          loaders: ['style', 'css', 'sass']
-        }
-      ]
-    }
-  }
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+
+const sassLoaders = [
+  "css-loader",
+  "autoprefixer-loader?browsers=last 2 version",
+  "sass-loader?indentedSyntax=sass&includePaths[]=" + path.resolve(__dirname, "./src"),
 ];
+
+module.exports = {
+  entry: {
+    app: ['./src/js/myapp']
+  },
+  module: {
+    loaders: [
+      { test: /\.(js|jsx)$/, loader: 'babel' },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!')) }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './dist'),
+    publicPath: '/dist',
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss'],
+    modulesDirectories: ['src', 'node_modules']
+  }
+};
