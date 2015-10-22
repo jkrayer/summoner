fc6c28ac6dd79c3250e0d9c75c5796af7c2ec6d0
jkrayer <jameskrayer@yahoo.com>
2015-10-22 18:10:38 -0400

    Rename event handlers. Add USE handler

--- src/js/monsterlistcontrol.jsx
+++ src/js/monsterlistcontrol.jsx
@@ -12,13 +12,17 @@ const MonsterListControl = React.createClass({
     let copy = (this.props.monsterID === this.props.visibleId) ? 'Hide' : 'Show';
     return (
       <div className={classes}>
-        <button type="button" className="button-reset" onClick={this._clickHandler}>{copy}</button>
+        <button type="button" className="button-reset" onClick={this._showHandler}>{copy}</button>
+        <button type="button" className="button-reset" onClick={this._useHandler}>Use</button>
       </div>
     );
   },
-  _clickHandler () {
+  _showHandler () {
     let theId = (this.props.monsterID === this.props.visibleId) ? -1 : this.props.monsterID;
     this.props.dispatch({type:'SHOW_MONSTER', id:theId});
+  },
+  _useHandler () {
+    this.props.dispatch({type:'USE_MONSTER', id:this.props.monsterID});
   }
 });
 
