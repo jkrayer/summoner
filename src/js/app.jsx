88feafc172e05b9e1aed40e0e9161d036800f067
jkrayer <jameskrayer@yahoo.com>
2015-10-22 21:51:45 -0400

    Add monster track

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import MonsterList from './monsterlist';
 import Monster from './monster';
+import MonsterTrack from './monstertrack';
 
 const App = React.createClass({
   propTypes: {
     allMonsters: React.PropTypes.array.isRequired,
     visibleStatBlock: React.PropTypes.object,
+    usedMonsters: React.PropTypes.array.isRequired,
     dispatch: React.PropTypes.func.isRequired
   },
   render () {
+    let usedMonster = this.props.usedMonsters.length ? <MonsterTrack monsters={this.props.usedMonsters} dispatch={this.props.dispatch} /> : null;
     let monster = this.props.visibleStatBlock ? <Monster monster={this.props.visibleStatBlock} /> : null;
     let visibleId = (this.props.visibleStatBlock) ? this.props.visibleStatBlock.id : -1;
     return (
       <div>
         <MonsterList allMonsters={this.props.allMonsters} dispatch={this.props.dispatch} visibleId={visibleId} />
+        {usedMonster}
         {monster}
       </div>
     );
