5ccb63f1a1089082ddb629ff0ccb25e3bc39930d
jkrayer <jkr@trustpilot.com>
2016-06-30 17:17:21 -0400

    Stub for new app direction

---
+++ src/app/main.jsx
@@ -0,0 +1,10 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('mountpoint')
+);
