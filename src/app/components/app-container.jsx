9ab93c023f898fb9efa16005010e916dd491ce1b
jkrayer <jameskrayer@yahoo.com>
2016-07-06 15:34:57 -0400

    Add skeleton for sample data

--- src/app/components/app-container.jsx
+++ src/app/components/app-container.jsx
@@ -2,10 +2,43 @@
 
 import React from 'react';
 import App from './app.jsx';
+import InitialData from './../data/initial-data.json';
 
 const AppContainer = React.createClass({
+  getInitialState () {
+    return {
+      monsters: null
+    };
+  },
+  getDefaultProps () {
+    return {
+      initialData: InitialData
+    };
+  },
+  handleLoadData () {},
+  handleSampleData () {
+    this.setState({
+      monsters: this.props.initialData
+    });
+  },
   render () {
-    return (<App />);
+    let app;
+    let { monsters } = this.state;
+
+    if (monsters) {
+      app = (<App monsters={monsters} />);
+    }
+    else {
+      // options use sample data
+      // load local data - <button type="button" className="btn btn-primary" onClick={this.handleLoadData}>{"Load Local Data"}</button>
+      app = (
+        <div className="">
+          <button type="button" className="btn" onClick={this.handleSampleData}>{"Use Sample Data"}</button>
+        </div>
+      );
+    }
+
+    return app;
   }
 });
 
