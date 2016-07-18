f44df5e2cbf787eb9911ead2ddd67b067272fda6
jkrayer <jkr@trustpilot.com>
2016-07-18 16:43:06 -0400

    Add Load data from file

--- src/app/components/load-data.jsx
+++ src/app/components/load-data.jsx
@@ -5,12 +5,27 @@ export default class LoadData extends React.Component {
   constructor(props) {
     super(props);
     this.handleSampleData = this.handleSampleData.bind(this);
+    this.handleLoadFile = this.handleLoadFile.bind(this);
   }
   handleSampleData() {
     this.props.dispatch({
       type: 'LOAD_DATA',
       monsters: InitialData
     });
+  }
+  handleLoadFile(event) {
+    const { dispatch } = this.props;
+    const files = event.target.files;
+    const file = files[0];
+    const reader = new FileReader();
+
+    reader.onload = function() {
+      dispatch({
+        type: 'LOAD_DATA',
+        monsters: JSON.parse(this.result)
+      });
+    }
+    reader.readAsText(file);
   }
   render() {
     return (
@@ -20,6 +35,10 @@ export default class LoadData extends React.Component {
           onClick={this.handleSampleData}
           type="button"
         >{"Use Sample Data"}</button>
+      <input
+        onChange={this.handleLoadFile}
+        type="file"
+      />
       </div>
     );
   }
