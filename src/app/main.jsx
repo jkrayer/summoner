0d84d3819d62c6133bb38310912fe140e18ddcb3
jkrayer <jkr@trustpilot.com>
2016-07-20 08:57:53 -0400

    Assign initial state in createStore

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -4,9 +4,16 @@ import AppContainer from './components/app-container.jsx';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import summonerApp from './redux/reducers';
+import InitialState from './redux/initial-state';
+
+// initialData = localstorage.get(dataID) || {};
+// let store = createStore(summonerApp, initialData, middleware());
+// function middleware() {
+//   on each event localstorage.save()
+// }
 
 // might be the proper place to set initial data, rather than app-container
-let store = createStore(summonerApp);
+let store = createStore(summonerApp, InitialState);
 
 ReactDOM.render(
   <Provider store={store}>
