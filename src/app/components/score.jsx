c62be52119078b097d38a2c0c8685ddb380a4c5d
jkrayer <jkr@trustpilot.com>
2016-07-27 15:33:08 -0400

    Fix lint errors

--- src/app/components/score.jsx
+++ src/app/components/score.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Score extends React.Component {
   setModifier() {
     const val = Math.floor(this.props.value / 2 - 5);
-    let operator = val > 0 ? '+' : '';
+    const operator = val > 0 ? '+' : '';
 
     return [' (', operator, val, ')'].join('');
   }
