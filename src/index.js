f5079e708590d075f4d1fd80ace79c1a1eac5881
jkrayer <jkr@trustpilot.com>
2016-08-30 16:29:34 -0400

    Add entry file

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/app.jsx';
+
+class App extends React.Component {
+  render () {
+    return (
+      <h1>{"JIM"}</h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
