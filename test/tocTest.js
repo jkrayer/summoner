0cef550b402f986a116eeb912c1840c09a3c53d3
jkrayer <jkr@trustpilot.com>
2016-09-23 13:04:59 -0400

    Refactor test

--- test/tocTest.js
+++ test/tocTest.js
@@ -4,26 +4,30 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import Toc from '../src/components/toc.jsx';
 
-describe('TOC component', function () {
+describe('<TOC />', function () {
   let el;
   let lis;
-  const data = [
-    { "name": "Aboleth" },
-    { "name": "Acolyte" },
-    { "name": "Adult Black Dragon" }
-  ];
+  const data = {
+    buttonEvent: function() {},
+    children: null,
+    data: [
+      { "name": "Aboleth" },
+      { "name": "Acolyte" },
+      { "name": "Adult Black Dragon" }
+    ]
+  };
 
   beforeEach(function () {
-    el = TestUtils.renderIntoDocument( React.createElement(Toc, { data }, null) );
+    el = TestUtils.renderIntoDocument( React.createElement(Toc, { buttonEvent: data.buttonEvent, data: data.data }, null) );
     lis = TestUtils.scryRenderedDOMComponentsWithTag(el, 'LI');
   });
 
   it('Should get data as props', function () {
-    assert.deepEqual(data, el.props.data);
+    assert.deepEqual(data, el.props);
   });
 
   it('Should render one LI element for each', function () {
-    assert.strictEqual(data.length, lis.length);
+    assert.strictEqual(3, lis.length);
   });
 
 });
