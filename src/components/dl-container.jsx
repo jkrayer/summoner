a12da1e1c0ca42293823a6c8f8f83db669f7b684
jkrayer <jkr@trustpilot.com>
2016-09-28 14:38:45 -0400

    Convert to pure function

--- src/components/dl-container.jsx
+++ src/components/dl-container.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import Dl from './dl.jsx';
+import Dl from './dl';
 
-export default class DlContainer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    let { data, hl } = this.props;
-    let terms = [];
+const DlContainer = function DlContainer(props) {
+  const { data, hl } = props;
+  const terms = [];
 
-    data.forEach((d) => {
-      terms.push(d.name);
-      terms.push(d.desc);
-    });
+  data.forEach((d) => {
+    terms.push(d.name);
+    terms.push(d.desc);
+  });
 
-    return (
-      <Dl data={terms} hl={hl} />
-    );
-  }
-}
-
-DlContainer.PropTypes = {
-  data: React.PropTypes.arrayOf(React.PropTypes.object)
+  return (
+    <Dl data={terms} hl={hl} />
+  );
 };
+
+DlContainer.propTypes = {
+  data: React.PropTypes.arrayOf(React.PropTypes.object),
+  hl: React.PropTypes.string
+};
+
+export default DlContainer;
