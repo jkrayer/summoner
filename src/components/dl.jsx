cb90fe640955d1da31128471fb233b1fc19f8c38
jkrayer <jkr@trustpilot.com>
2016-09-28 14:48:40 -0400

    Conver to pure function

--- src/components/dl.jsx
+++ src/components/dl.jsx
@@ -1,37 +1,33 @@
 import React from 'react';
-import style from '../style/monster.css'
-import Marked from 'marked';
+import marked from 'marked';
+import style from '../style/monster.css';
 
-export default class Dl extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    let markup;
-    let { data, hl } = this.props;
-    let terms = data.map((term, index) => {
-      let i = index % 2;
-      let key = i.toString() + index;
-      if (i === 0) {
-        return <dt key={key}>{term}</dt>;
-      }
-      markup = Marked(term, { sanitize: false });
-      return <dd dangerouslySetInnerHTML={{ __html: markup }} key={key} />;
-    });
-    let headline = hl ? <h2 className={style['monster-section']}>{hl}</h2> : null;
+const Dl = function Dl(props) {
+  const { data, hl } = props;
+  const headline = hl ? <h2 className={style['monster-section']}>{hl}</h2> : null;
+  const terms = data.map((term, index) => {
+    const i = index % 2;
+    const key = i.toString() + index;
+    const markup = marked(term, { sanitize: false });
+    if (i === 0) {
+      return <dt key={key}>{term}</dt>;
+    }
+    return <dd dangerouslySetInnerHTML={{ __html: markup }} key={key} />;
+  });
 
-    return (
-      <div>
-        {headline}
-        <dl className={style['stat-defs-lg']}>
-          { terms }
-        </dl>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {headline}
+      <dl className={style['stat-defs-lg']}>
+        { terms }
+      </dl>
+    </div>
+  );
+};
 
-Dl.PropTypes = {
+Dl.propTypes = {
   data: React.PropTypes.arrayOf(React.PropTypes.object),
   hl: React.PropTypes.string
 };
+
+export default Dl;
