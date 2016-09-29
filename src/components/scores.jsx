f9befc264b356ed32e69e834062d0fa85962d353
jkrayer <jameskrayer@yahoo.com>
2016-09-29 10:08:28 -0400

    Convert to pure function

--- src/components/scores.jsx
+++ src/components/scores.jsx
@@ -1,43 +1,38 @@
 import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
-import Score from './score.jsx'
-import style from '../style/table.css'
+import Score from './score';
+import style from '../style/table.css';
 
-export default class Scores extends React.Component {
-  constructor(props) {
-    super(props);
-    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-  }
-  render() {
-    const { scores } = this.props;
+const Scores = function Scores(props) {
+  const { scores } = props;
 
-    return (
-      <table className={style['score-table']}>
-        <thead>
-          <tr>
-            <th>{"STR"}</th>
-            <th>{"DEX"}</th>
-            <th>{"CON"}</th>
-            <th>{"INT"}</th>
-            <th>{"WIS"}</th>
-            <th>{"CHA"}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><Score score={scores.strength} /></td>
-            <td><Score score={scores.dexterity} /></td>
-            <td><Score score={scores.constitution} /></td>
-            <td><Score score={scores.intelligence} /></td>
-            <td><Score score={scores.wisdom} /></td>
-            <td><Score score={scores.charisma} /></td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-}
+  return (
+    <table className={style['score-table']}>
+      <thead>
+        <tr>
+          <th>{'STR'}</th>
+          <th>{'DEX'}</th>
+          <th>{'CON'}</th>
+          <th>{'INT'}</th>
+          <th>{'WIS'}</th>
+          <th>{'CHA'}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><Score score={scores.strength} /></td>
+          <td><Score score={scores.dexterity} /></td>
+          <td><Score score={scores.constitution} /></td>
+          <td><Score score={scores.intelligence} /></td>
+          <td><Score score={scores.wisdom} /></td>
+          <td><Score score={scores.charisma} /></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
 Scores.propTypes = {
   scores: React.PropTypes.objectOf(React.PropTypes.number).isRequired
 };
+
+export default Scores;
