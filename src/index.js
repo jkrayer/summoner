32dc88dc6aaf47fb5bfeead546233d5bb61f8711
jkrayer <jameskrayer@yahoo.com>
2016-09-29 14:59:14 -0400

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+/* global document:true */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import data from './data/5e-SRD-Monsters';
-import App from './components/app.jsx';
+import App from './components/app';
 
 ReactDOM.render(
-  <App data={data} />,
+  React.createElement(App, { data }, null),
   document.getElementById('mountpoint')
 );
