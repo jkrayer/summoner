ced08347e295bce154f5db188aacadb5c767e293
jkrayer <jameskrayer@yahoo.com>
2016-09-29 20:00:18 -0400

    Fix test because component refactor

--- test/tocTest.js
+++ test/tocTest.js
@@ -7,27 +7,18 @@ import Toc from '../src/components/toc.jsx';
 describe('<TOC />', function () {
   let el;
   let lis;
-  const data = {
-    buttonEvent: function() {},
-    children: null,
-    data: [
-      { "name": "Aboleth" },
-      { "name": "Acolyte" },
-      { "name": "Adult Black Dragon" }
-    ]
-  };
+  const data = [
+    { "name": "Aboleth" },
+    { "name": "Acolyte" },
+    { "name": "Adult Black Dragon" }
+  ];
 
   beforeEach(function () {
-    el = TestUtils.renderIntoDocument( React.createElement(Toc, { buttonEvent: data.buttonEvent, data: data.data }, null) );
-    lis = TestUtils.scryRenderedDOMComponentsWithTag(el, 'LI');
-  });
-
-  it('Should get data as props', function () {
-    assert.deepEqual(data, el.props);
+    el = Toc({ buttonEvent: data.buttonEvent, data: data.data });
   });
 
   it('Should render one LI element for each', function () {
-    assert.strictEqual(3, lis.length);
+    assert.strictEqual(3, el.props.children.length);
   });
 
 });
