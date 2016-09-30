3ba572c5132f81047c62b809621e89c8ae00b893
jkrayer <jameskrayer@yahoo.com>
2016-09-29 23:22:43 -0400

    Add <TocContainer/> test

---
+++ test/tocContainerTest.js
@@ -0,0 +1,32 @@
+require('./testdom')('<html><body></body></html>');
+import { assert } from 'chai';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import TocContainer from '../src/components/toc-container.jsx';
+import testData from './testdata';
+import filteredTestData from './filteredtestdata';
+
+describe('<TocContainer />', function () {
+  let el;
+  function buttonEvent () {
+    return true;
+  }
+
+  beforeEach(function () {
+    el = TestUtils.renderIntoDocument( React.createElement(TocContainer, { buttonEvent, data: testData }, null) );
+  });
+
+  it('onFilterChange should not accept characters other than A-Z, - and \'', function () {
+    el.onFilterChange({target:{ value: '9'}});
+    assert.deepEqual(el.state.filteredData, testData);
+    el.onFilterChange({target:{ value: '^'}});
+    assert.deepEqual(el.state.filteredData, testData);
+    el.onFilterChange({target:{ value: '@'}});
+    assert.deepEqual(el.state.filteredData, testData);
+  });
+
+//  it('', function () {
+//    el.onFilterChange({target:{ value: 'dragon'}});
+//    assert.deepEqual(el.state.filteredData, filteredTestData);
+//  });
+});
