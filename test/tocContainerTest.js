27e1151f2d575f8cd40a321493676ae745f5b236
jkrayer <jkr@trustpilot.com>
2016-09-30 13:27:03 -0400

    Test `type` and `subtype` search.

--- test/tocContainerTest.js
+++ test/tocContainerTest.js
@@ -5,6 +5,7 @@ import TestUtils from 'react-addons-test-utils';
 import TocContainer from '../src/components/toc-container.jsx';
 import testData from './testdata';
 import filteredTestData from './filteredtestdata';
+import demonData from './demonData';
 
 describe('<TocContainer />', function () {
   let el;
@@ -33,16 +34,32 @@ describe('<TocContainer />', function () {
   });
 
   it('onFilterByChange should set state.filterBy to an approved value', function () {
-    //el.onFilterByChange({target:{ value: 'challenge_rating'}});
-    //assert.inArray(el.state.filterBy, [name, challenge_rating, type, subtype, terrain]);
-    //el.onFilterByChange({target:{ value: 'dragon'}});
-    //assert.!inArray(el.state.filterBy, [name, challenge_rating, type, subtype, terrain]);
+    el.onFilterByChange({target:{ value: 'challenge_rating'}});
+    assert.include(el.validFilters, el.state.filterBy);
+    el.onFilterByChange({target:{ value: 'dragon'}});
+    assert.strictEqual(el.state.filterBy, 'name');
   });
 
-  it('onFilterChange should return two dragons', function () {
-    //el.onFilterChange({target:{ value: '14'}});
-    //setTimeout(function(){
-    //  assert.deepEqual(el.state.filteredData, filteredTestData[0]);
-    //}, 20);
+  it('filter by `challenge_rating` should return 1 result', function () {
+    el.onFilterChange({target:{ value: '14'}});
+    setTimeout(function(){
+      assert.deepEqual(el.state.filteredData, filteredTestData[0]);
+    }, 20);
+  });
+
+  it('filter by `type` should return 1 result', function () {
+    el.onFilterByChange({target:{ value: 'type'}});
+    el.onFilterChange({target:{ value: 'fiend'}});
+    setTimeout(function(){
+      assert.deepEqual(el.state.filteredData, demonData);
+    }, 20);
+  });
+
+  it('filter by `subtype` should return 1 result', function () {
+    el.onFilterByChange({target:{ value: 'subtype'}});
+    el.onFilterChange({target:{ value: 'demon'}});
+    setTimeout(function(){
+      assert.deepEqual(el.state.filteredData, demonData);
+    }, 20);
   });
 });
