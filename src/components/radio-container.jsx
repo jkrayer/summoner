eb3fd955487d9a61b7ea937e90abe9a4fe1f2c52
jkrayer <jkr@trustpilot.com>
2016-10-03 14:28:30 -0400

    Convert to pure function

--- src/components/radio-container.jsx
+++ src/components/radio-container.jsx
@@ -2,31 +2,26 @@ import React from 'react';
 import Radio from './radio';
 import style from '../style/radio.css';
 
-export default class RadioContainer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const { checked, event, legend, name, radios } = this.props;
-    const radioButtons = radios.map((r, index) =>
-      <Radio
-        checked={checked === r.val}
-        event={event}
-        key={index}
-        label={r.label}
-        name={name}
-        val={r.val}
-      />
-    );
-    return (
-      <div
-        className={style['radio-container']}
-        title={legend}
-      >
-        {radioButtons}
-      </div>
-    );
-  }
+export default function RadioContainer(props) {
+  const { checked, event, legend, name, radios } = props;
+  const radioButtons = radios.map((r, index) =>
+    <Radio
+      checked={checked === r.val}
+      event={event}
+      key={index}
+      label={r.label}
+      name={name}
+      val={r.val}
+    />
+  );
+  return (
+    <div
+      className={style['radio-container']}
+      title={legend}
+    >
+      {radioButtons}
+    </div>
+  );
 }
 
 RadioContainer.propTypes = {
@@ -34,13 +29,8 @@ RadioContainer.propTypes = {
   event: React.PropTypes.func.isRequired,
   legend: React.PropTypes.string,
   name: React.PropTypes.string.isRequired,
-  radios: React.PropTypes.arrayOf(
-    React.PropTypes.shape({
-      val: React.PropTypes.string,
-      label: React.PropTypes.string
-    })
-  ).isRequired
-}
+  radios: React.PropTypes.arrayOf({}).isRequired
+};
 
 RadioContainer.defaultProps = {
   checked: null,
