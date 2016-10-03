6706d92f6bab64006ffb9a1c2b1c59e3fe6cab52
jkrayer <jkr@trustpilot.com>
2016-10-03 14:22:45 -0400

    Fix lint error, stylesheet inclusion

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import TocContainer from './toc-container';
 import Monster from './monster';
-import style from '../style/global.css';
 
 export default class App extends React.Component {
   constructor(props) {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import data from './data/5e-SRD-Monsters';
 import App from './components/app';
+import style from './style/global.css';
 
 ReactDOM.render(
   React.createElement(App, { data }, null),
