7af935e9b5712f8f4f585661b37d0be594e7885f
jkrayer <jameskrayer@yahoo.com>
2016-10-16 09:27:42 -0400

    Fix lint errors

--- src/components/encounter.jsx
+++ src/components/encounter.jsx
@@ -4,15 +4,30 @@ import Monster from './monster';
 import helpers from '../utilities/helpers';
 import style from '../style/toc.css';
 
-export default class Encounter extends React.Component  {
-  constructor(props) {
-    super(props);
+function renderHpBlocks(monster) {
+  const lis = monster.hpPerMonster.map((hp, index) =>
+    (
+    <li key={index}>
+      <span>{hp}</span>
+      <input type="text" />
+    </li>
+    )
+  );
+  return (
+    <ol>
+      {lis}
+    </ol>
+  );
+}
+
+export default class Encounter extends React.Component {
+  constructor() {
+    super();
     this.renderMonsterBlock = this.renderMonsterBlock.bind(this);
-    this.renderHpBlocks = this.renderHpBlocks.bind(this);
   }
   renderMonsterBlock(monster) {
     const { setSelectedMonster } = this.props;
-    const blocks = this.renderHpBlocks(monster);
+    const blocks = renderHpBlocks(monster);
     return (
       <section key={monster.arrayIndex}>
         <h2>
@@ -25,22 +40,6 @@ export default class Encounter extends React.Component  {
         </h2>
         {blocks}
       </section>
-    );
-  }
-  renderHpBlocks(monster) {
-    console.log(19, monster);
-    const lis = monster.hpPerMonster.map((hp, index) => {
-      return (
-        <li key={index}>
-          <span>{hp}</span>
-          <input type="text" />
-        </li>
-      );
-    });
-    return (
-      <ol>
-        {lis}
-      </ol>
     );
   }
   render() {
@@ -62,4 +61,4 @@ Encounter.propTypes = {
   monsters: React.PropTypes.arrayOf(React.PropTypes.object),
   selectedMonster: React.PropTypes.shape(),
   setSelectedMonster: React.PropTypes.func
-}
+};
