821f725a5ddbce5a2a19b180dffd9b66b54ae115
jkrayer <jameskrayer@yahoo.com>
2016-10-30 13:08:07 -0400

    Pass deleteMonster

--- src/components/encounter.jsx
+++ src/components/encounter.jsx
@@ -3,11 +3,18 @@ import HpTrack from './hptrack';
 import Monster from './monster';
 
 export default function Encounter(props) {
-  const { calculateHp, monsters, selectedMonster, setSelectedMonster } = props;
+  const {
+    calculateHp,
+    deleteMonster,
+    monsters,
+    selectedMonster,
+    setSelectedMonster
+  } = props;
   return (
     <div>
       <HpTrack
         calculateHp={calculateHp}
+        deleteMonster={deleteMonster}
         monsters={monsters}
         setSelectedMonster={setSelectedMonster}
       />
@@ -21,6 +28,7 @@ export default function Encounter(props) {
 
 Encounter.propTypes = {
   calculateHp: React.PropTypes.func,
+  deleteMonster: React.PropTypes.func,
   monsters: React.PropTypes.arrayOf(React.PropTypes.object),
   selectedMonster: React.PropTypes.shape(),
   setSelectedMonster: React.PropTypes.func
