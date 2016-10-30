181e7cd8c913a5250b9468a94bd8686e3b914620
jkrayer <jameskrayer@yahoo.com>
2016-10-30 13:23:30 -0400

    Add delete button and function

--- src/components/hptrack.jsx
+++ src/components/hptrack.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import HpBlock from './hpblock';
 import Button from './button';
+import ButtonClose from './button-close';
 import helpers from '../utilities/helpers';
-// import style from '../style/hptrack.css';
+import style from '../style/hptrack.css';
 
 export default function HpTrack(props) {
-  const { calculateHp, setSelectedMonster } = props;
+  const { calculateHp, deleteMonster, setSelectedMonster } = props;
   const blocks = props.monsters.map((monster, index) =>
     <section key={monster.arrayIndex}>
       <h2>
-        <Button event={() => {}}>Clear Monster</Button>
         <Button
-          className="{style.btn}"
+          className={style.btn}
           event={helpers.partialApply(setSelectedMonster, monster.arrayIndex)}
         >
           {monster.name}
         </Button>
+        <ButtonClose event={() => deleteMonster(monster)} />
       </h2>
       <HpBlock
         monster={monster}
@@ -34,6 +35,7 @@ export default function HpTrack(props) {
 
 HpTrack.propTypes = {
   calculateHp: React.PropTypes.func,
+  deleteMonster: React.PropTypes.func,
   monsters: React.PropTypes.arrayOf(
     React.PropTypes.shape({})
   ),
