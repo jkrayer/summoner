4e3f79757dfbca2292b0ce84e8daddfb1ce9236e
jkrayer <jameskrayer@yahoo.com>
2016-10-30 21:59:01 -0400

    Add XpCalculator component

---
+++ src/components/xp-calculator.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Input from './input';
+import style from '../style/xp-calculator.css';
+
+export default class XpCalculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      split: null,
+      total: 0
+    };
+    this.calculateXpReward = this.calculateXpReward.bind(this);
+  }
+  componentWillMount() {
+    const { monsters } = this.props;
+    let total = 0;
+    monsters.forEach(monster => {
+      total += monster.xp * monster.hpPerMonster.length;
+    });
+    this.setState({
+      total
+    })
+  }
+  calculateXpReward(val) {
+    const divisor = parseInt(val, 10);
+    let split = 0;
+    if (isNaN(divisor)) {
+      split = null;
+    } else {
+      split = ['=', Math.round(this.state.total / divisor), 'per player'].join(' ');
+    }
+    this.setState({
+      split
+    });
+  }
+  render() {
+    const { split, total } = this.state;
+    return(
+      <div className={style.calc}>
+        Encounter XP: {total} /
+        <Input
+          blurEvent={this.calculateXpReward}
+          placeholder="# of Players"
+        />
+        {split}
+      </div>
+    );
+  }
+}
+
+XpCalculator.propTypes = {
+  monsters: React.PropTypes.arrayOf(
+    React.PropTypes.shape({})
+  )
+};
