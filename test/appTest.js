2956060e0637692c17edb695c2531d7cfa792ce7
jkrayer <jameskrayer@yahoo.com>
2016-11-03 08:39:03 -0400

    Add tests

--- test/appTest.js
+++ test/appTest.js
@@ -7,21 +7,26 @@ import testData from './testdata';
 
 describe('<App />', function () {
   let el;
+  const monster = {
+    arrayIndex: 7,
+    hpPerMonster: [39, 39, 39, 39],
+    name: 'Orc',
+    xp: 100
+  };
+
 
   beforeEach(function () {
-    el = TestUtils.renderIntoDocument( React.createElement(App, { data: testData }, null) );
+    el = TestUtils.renderIntoDocument(
+      React.createElement(
+        App,
+        { data: testData, location: { pathname: '/testPane' } },
+        React.createElement('div', {}, null)
+      )
+    );
   });
 
-  it('It should have a default state.selectedMonster = null', function () {
-    assert.isNull(el.state.selectedMonster);
-  });
-
-//  it('It should set a new monester when a valid key is given', function () {
-//    el.setSelectedMonster(2);
-//    assert.deepEqual(data[2], el.state.selectedMonster);
-//  });
-
-  it('It should set null if an invalid key is given', function () {
+  // Test setSelectedMonster
+  it('setSelectedMonster should set null if an invalid key is given', function () {
     el.setSelectedMonster(-1);
     assert.isNull(el.state.selectedMonster);
     el.setSelectedMonster(1000);
@@ -30,6 +35,7 @@ describe('<App />', function () {
     assert.isNull(el.state.selectedMonster);
   });
 
+  // Test toggleAny
   it('It should toggle the supplied boolean', function () {
     el.setSelectedMonster(0);
     el.toggleAny('showAddWindow');
@@ -38,15 +44,28 @@ describe('<App />', function () {
     assert.strictEqual(el.state.showAddWindow, false);
   });
 
+  // Test addMonster
   it('It should add a monster object', function () {
-    const monster = {
-      arrayIndex: 7,
-      hpPerMonster: 39,
-      numMonsters: 2
-    };
     el.setSelectedMonster(0);
     el.addMonster(monster);
     assert.deepEqual(el.state.monsters[0], monster);
   });
 
+  // Test calculateHp
+  it('It should return false when if newVal is NaN', function () {
+    assert.isFalse(el.calculateHp(0, 1, 'a'));
+  });
+
+  it('It should subtract from the second hp block', function () {
+    el.addMonster(monster);
+    el.calculateHp(0, 1, 7);
+    assert.strictEqual(el.state.monsters[0].hpPerMonster[1], 32);
+  });
+
+  // Test deleteMonster
+  it('It should delete a monster', function () {
+    el.addMonster(monster);
+    el.deleteMonster(monster);
+    assert.deepEqual(el.state.monsters, []);
+  });
 });
