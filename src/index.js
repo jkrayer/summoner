6f37dad8f0fcbd8adec8583695fd7d8dc1737070
James Krayer <jkrayer@ABT-JK-H03461.local>
2016-12-15 16:06:09 -0500

    Add the missing piece

--- src/index.js
+++ src/index.js
@@ -2,18 +2,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router'
-import App from './components/app';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 
+//import App from './components/app';
 import Home from './components/home';
 import Encounter from './components/encounter';
+import appStore from './redux/reducers';
 
 import style from './style/global.css';
 
-ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="/encounter" component={Encounter} />
-    </Route>
-  </Router>
-), document.getElementById('mountpoint'));
+
+// Actions
+const ADD_TODO = 'ADD_TODO';
+
+// Action Creator
+function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    text
+  }
+}
+
+// Bound Action Creator
+const boundAddTodo = (text) => dispatch(addTodo(text));
+
+// Reducer
+const initialState = {
+  visibilityFilter: 'None',
+  todos: []
+}
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return Object.assign({}, state, {todos: [...state.todos, {text: action.text, completed: false}]})
+      break;
+    default:
+      return state
+      break;
+  }
+}
+
+// Store
+let store = createStore(todoApp);
+
+function App(props) {
+console.log(41, props);
+  return(
+    <h1>Hello World</h1>
+  );
+}
+
+function select(state) {
+  return state;
+}
+
+connect(select)(App);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('mountpoint')
+);
+
+//ReactDOM.render((
+//  <Provider store={store}>
+//    <Router history={hashHistory}>
+//      <Route path="/" component={App}>
+//        <IndexRoute component={Home} />
+//        <Route path="/encounter" component={Encounter} />
+//      </Route>
+//    </Router>
+//  </Provider>
+//), document.getElementById('mountpoint'));
