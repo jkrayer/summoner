bf26b8c12227b9227a6804fcc08518480333e912
jkrayer <jameskrayer@yahoo.com>
2016-12-15 18:11:22 -0500

    Leave the gun, take the canoles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
 
-//import App from './components/app';
+import App from './components/app';
 import Home from './components/home';
 import Encounter from './components/encounter';
 import appStore from './redux/reducers';
@@ -13,73 +13,59 @@ import appStore from './redux/reducers';
 import style from './style/global.css';
 
 
-// Actions
-const ADD_TODO = 'ADD_TODO';
+// Actions *****
+//const ADD_TODO = 'ADD_TODO';
 
 // Action Creator
-function addTodo(text) {
-  return {
-    type: ADD_TODO,
-    text
-  }
-}
-
-// Bound Action Creator
-const boundAddTodo = (text) => dispatch(addTodo(text));
-
-// Reducer
-const initialState = {
-  visibilityFilter: 'None',
-  todos: []
-}
-
-function todoApp(state = initialState, action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return Object.assign({}, state, {todos: [...state.todos, {text: action.text, completed: false}]})
-      break;
-    default:
-      return state
-      break;
-  }
-}
-
-// Store
-let store = createStore(todoApp);
-
-function App(props) {
-console.log(41, props);
-  return(
-    <h1>Hello World: {props.visibilityFilter}</h1>
-  );
-}
-
-
-const mapStateToProps = (state) => {
-  return state;
-}
-
-
-//function select(state) {
-//  return state;
+//function addTodo(text) {
+//  return {
+//    type: ADD_TODO,
+//    text
+//  }
 //}
 
-const ConApp = connect(mapStateToProps)(App);
+// Bound Action Creator
+//const boundAddTodo = (text) => dispatch(addTodo(text));
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ConApp />
-  </Provider>
-  , document.getElementById('mountpoint')
-);
+// Reducers *****
+//const initialState = {
+//  visibilityFilter: 'None',
+//  todos: []
+//}
+//
+//function todoApp(state = initialState, action) {
+//  switch (action.type) {
+//    case ADD_TODO:
+//      return Object.assign({}, state, {todos: [...state.todos, {text: action.text, completed: false}]})
+//      break;
+//    default:
+//      return state
+//      break;
+//  }
+//}
 
-//ReactDOM.render((
+// Store *****
+//let store = createStore(todoApp);
+
+
+// Connect *****
+//const mapStateToProps = (state) => {
+//  return state;
+//}
+//const ConApp = connect(mapStateToProps)(App);
+
+//ReactDOM.render(
 //  <Provider store={store}>
-//    <Router history={hashHistory}>
-//      <Route path="/" component={App}>
-//        <IndexRoute component={Home} />
-//        <Route path="/encounter" component={Encounter} />
-//      </Route>
-//    </Router>
+//    <ConApp />
 //  </Provider>
-//), document.getElementById('mountpoint'));
+//  , document.getElementById('mountpoint')
+//);
+
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="/encounter" component={Encounter} />
+    </Route>
+  </Router>
+), document.getElementById('mountpoint'));
