71138730e896fca930b29b3b261f58576e6081b1
jkrayer <jameskrayer@yahoo.com>
2016-12-21 09:41:54 -0500

    Add addMonster

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { initialState } from '../state';
-import { SET_SELECTED_MONSTER,
-         SET_MODAL_VISABILITY,
-         SET_MONSTER_PANE_VISABILITY
-       } from '../actions';
+import { SET_SELECTED_MONSTER, SET_MODAL_VISABILITY, SET_MONSTER_PANE_VISABILITY, ADD_ENCOUNTER_MONSTER } from '../actions/action-constants';
 
 export default function appData(state = initialState, action) {
+  var encounter;
   switch (action.type) {
     case SET_SELECTED_MONSTER:
       return Object.assign({}, state, { selectedMonster: action.data });
@@ -15,6 +13,14 @@ export default function appData(state = initialState, action) {
     case SET_MONSTER_PANE_VISABILITY:
       return Object.assign({}, state, { monsterPaneIsVisable: action.data });
       break;
+    case ADD_ENCOUNTER_MONSTER:
+      encounter = [].concat(state.encounter, [action.data]);
+      return Object.assign({}, state, {encounter});
+      break;
+//    case DEL_MONSTER_PANE_VISABILITY:
+//      encounter = state.encounter.slice(index, 1);
+//      return Object.assign({}, state, {encounter});
+//      break;
     default:
       return state;
       break;
