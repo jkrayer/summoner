c1915a7b0d58dfb99fd1fe4ee1933ad6dd36f121
jkrayer <jameskrayer@yahoo.com>
2016-12-25 16:40:38 -0500

    Use `encounter` instead of `monsters`

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,37 +5,15 @@ import { SET_SELECTED_MONSTER, SET_MONSTER_PANE_VISABILITY } from '../redux/acti
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showAddWindow: false,
-      showConfirmWindow: false
-    };
-    this.setSelectedMonster = this.setSelectedMonster.bind(this);
-    this.toggleAny = this.toggleAny.bind(this);
     this.calculateHp = this.calculateHp.bind(this);
     this.deleteMonster = this.deleteMonster.bind(this);
   }
-  setSelectedMonster(key) {
-    const { dispatch, monsters } = this.props;
-    dispatch({
-      type: SET_SELECTED_MONSTER,
-      data: monsters[key] || null
-    });
-    dispatch({
-      type: SET_MONSTER_PANE_VISABILITY,
-      data: true
-    });
-  }
   deleteMonster(monster) {
-    const { monsters } = this.state;
+    const { encounter } = this.state;
     const newMonsters = monsters.filter(m => m.arrayIndex !== monster.arrayIndex);
     this.setState({
       monsters: newMonsters
     });
-  }
-  toggleAny(key) {
-    const obj = {};
-    obj[key] = !this.state[key];
-    this.setState(obj);
   }
   calculateHp(monsterIndex, hpIndex, newVal) {
     const operator = (newVal[0] === '+') ? '+' : '-';
@@ -51,27 +29,17 @@ export default class App extends React.Component {
     });
   }
   render() {
-    const { showAddWindow, showConfirmWindow } = this.state;
-    const { monsters } = this.props;
-    const propsToPass = {
-      monsters,
-      setSelectedMonster: this.setSelectedMonster
-    };
+    const { encounter } = this.props;
+    const propsToPass = {};
     let link = null;
 
     if (this.props.location.pathname !== '/') {
       link = <IndexLink to="/">Home</IndexLink>;
-    } else if (monsters.length) {
+    } else if (encounter.length) {
       link = <Link to="/encounter">Encounter</Link>;
     }
 
     switch (this.props.location.pathname) {
-      case '/':
-        propsToPass.data = monsters;
-        propsToPass.showAddWindow = showAddWindow;
-        propsToPass.showConfirmWindow = showConfirmWindow;
-        propsToPass.toggleShowWindow = () => this.toggleAny('showConfirmWindow');
-        break;
       case '/encounter':
         propsToPass.calculateHp = this.calculateHp;
         propsToPass.deleteMonster = this.deleteMonster;
@@ -94,7 +62,7 @@ App.propTypes = {
   location: React.PropTypes.shape({
     pathname: React.PropTypes.string.isRequired
   }).isRequired,
-  monsters: React.PropTypes.arrayOf(
+  encounter: React.PropTypes.arrayOf(
     React.PropTypes.shape({})
   )
 };
--- src/components/c_app.js
+++ src/components/c_app.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => {
   return {
-    monsters: state.monsters
+    encounter: state.encounter
   };
 }
 
