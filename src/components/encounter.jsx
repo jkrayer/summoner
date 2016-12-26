da569cf109cb15278382f1acfb903c5a8cd38c49
jkrayer <jameskrayer@yahoo.com>
2016-12-25 23:17:46 -0500

    Remove components

--- src/components/encounter.jsx
+++ src/components/encounter.jsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import HpTrack from './hptrack';
+import HpTrackContainer from './c_hptrack';
 import MonsterContainer from './c_monster';
 
+
 export default function Encounter(props) {
-  const {
-    calculateHp,
-    deleteMonster,
-    monsters,
-    selectedMonster,
-    setSelectedMonster
-  } = props;
   return (
     <div>
-      <HpTrack
-        calculateHp={calculateHp}
-        deleteMonster={deleteMonster}
-        monsters={monsters}
-        setSelectedMonster={setSelectedMonster}
-      />
+      <HpTrackContainer />
       <MonsterContainer />
     </div>
   );
 }
-
-Encounter.propTypes = {
-  calculateHp: React.PropTypes.func,
-  deleteMonster: React.PropTypes.func,
-  monsters: React.PropTypes.arrayOf(React.PropTypes.object),
-  selectedMonster: React.PropTypes.shape(),
-  setSelectedMonster: React.PropTypes.func
-};
