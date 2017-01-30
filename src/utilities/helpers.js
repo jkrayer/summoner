b21d6900a754a79e24d8810a8edcd8c9222e51af
jkrayer <jameskrayer@yahoo.com>
2017-01-29 22:35:09 -0500

    Add loadState and saveState methods

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,15 +1,32 @@
-function getXp(cr) {
+export function getXp(cr) {
   return parseInt(/\([,\d]*/g.exec(cr)[0].slice(1).replace(',', ''), 10);
 }
 
-function partialApply(func) {
+export function partialApply(func) {
   var args = [].slice.call(arguments, 1);
   return function part() {
     func.apply(null, args.concat( [].slice.call(arguments) ));
   };
 }
 
-export default {
-  getXp,
-  partialApply
-};
+export function loadState() {
+  try {
+    const savedState = localstorage.getItem('summoner');
+    if (savedState === null) {
+      return undefined;
+    }
+    return JSON.parse(savedState);
+  } catch (err) {
+    console.error('Error retrieving saved state: ' + err);
+    return undefined;
+  }
+}
+
+export function saveState(state) {
+  try {
+    const saveState = JSON.stringify(state);
+    localstorage.saveState('summoner', saveState);
+  } catch (err) {
+    console.error('Error saving state: ' + err);
+  }
+}
